f6398906d9eb358c1f2fb88cc33e5409c0a670dd
HusnaFatima-Mf <husna15fatima@gmail.com>
2026-04-18 22:55:12 +0530

    Add route to get book details by ISBN
    Implement endpoint to retrieve book details by ISBN.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -21,6 +21,21 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
+  const isbn = req.params.isbn;
+
+    const book = books[isbn];
+
+    if (book) {
+
+        return res.status(200).json(JSON.stringify(book));
+
+    } else {
+
+        return res.status(404).json({ message: "Book not found" });
+
+    }
+
+});
   return res.status(300).json({message: "Yet to be implemented"});
  });
   
